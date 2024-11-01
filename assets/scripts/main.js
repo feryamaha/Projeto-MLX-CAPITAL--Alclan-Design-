e1cd2bd839a3390ad0a895e773567998c5249690
Fernando Moreira <feryamaha@hotmail.com>
2024-10-31 21:28:51 -0300

    7 commit criação

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const scrollerContent = document.querySelector('.scroller-content');
+    const scrollerContents = document.querySelectorAll('.scroller-content');
     const contentList = [
-
-        { tag: 'h3', text: 'Expertise no setor de saúde não assistencial' },
-        { tag: 'h5', text: 'MLX Capital' },
-        { tag: 'h3', text: 'Histórico de transações' },
-        { tag: 'h5', text: 'MLX Capital' },
-        { tag: 'h3', text: 'Rede de relacionamento com stakeholders relevantes' },
-        { tag: 'h5', text: 'MLX Capital' },
         { tag: 'h3', text: 'Expertise no setor de saúde não assistencial' },
         { tag: 'h5', text: 'MLX Capital' },
         { tag: 'h3', text: 'Histórico de transações' },
@@ -16,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         { tag: 'h5', text: 'MLX Capital' }
     ];
 
-    function insertContent() {
+    function insertContent(scrollerContent) {
         contentList.forEach(item => {
             const element = document.createElement(item.tag);
             element.textContent = item.text;
@@ -43,24 +36,30 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    insertContent();
+    scrollerContents.forEach(insertContent);
 
-    let scrollAmount = 0;
-    const scrollSpeed = 0.3;
-    const itemWidth = scrollerContent.children[0].offsetWidth + 48; // Largura do item + margem
+    scrollerContents.forEach(scrollerContent => {
+        let scrollAmount = 0;
+        const scrollSpeed = 1;
+        const itemWidth = scrollerContent.children[0].offsetWidth + 48; // Largura do item + margem
 
-    function scrollContinuously() {
-        scrollAmount -= scrollSpeed;
-        scrollerContent.style.transform = `translateX(${scrollAmount}px)`;
-        if (Math.abs(scrollAmount) >= itemWidth) {
-            scrollAmount = 0;
-            scrollerContent.appendChild(scrollerContent.firstElementChild); // Mover o primeiro item para o fim
+        function scrollContinuously() {
+            scrollAmount -= scrollSpeed;
+            scrollerContent.style.transform = `translateX(${scrollAmount}px)`;
+            if (Math.abs(scrollAmount) >= itemWidth) {
+                scrollAmount = 0;
+                scrollerContent.appendChild(scrollerContent.firstElementChild); // Mover o primeiro item para o fim
+            }
+            requestAnimationFrame(scrollContinuously);
         }
-        requestAnimationFrame(scrollContinuously);
-    }
 
-    scrollContinuously();
+        scrollContinuously();
+    });
 });
+
+
+
+
 
 
 
